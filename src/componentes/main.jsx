51395a77a894517896b206feb1d7502e83a95690
FedeGomboc <47128235@est.ort.edu.ar>
2023-06-11 23:42:29 -0300

    avanzando pagina inicial

--- src/componentes/main.jsx
+++ src/componentes/main.jsx
@@ -1,64 +1,113 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
-import Constants from 'expo-constants'
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  Pressable,
+} from "react-native";
+import Constants from "expo-constants";
+import { useFonts } from "expo-font"
 
-const Main = (props) => {
+const Main = () => {
 
-    const {onPress} = props
+  const [fuentesCargadas] = useFonts({
+    "Fredoka Semibold": require("../assets/fonts/Fredoka-SemiBold.ttf"),
+    "Fredoka Regular": require("../assets/fonts/Fredoka-Regular.ttf"),
+    "Inter Regular": require("../assets/fonts/Inter-Regular.ttf")
+  })
+  
+  if (!fuentesCargadas) {
+    return null
+  }
 
-    return (
-        <View style={styles.container}>
-        <Text style={styles.titulo}> <Text style={styles.mostaza}>Easy</Text><Text style={styles.verde}>Eating</Text></Text>
-        <Text style={styles.subtitulo}>Escaneá, conocé, disfrutá</Text>
-        {/* <Button title="Iniciar sesión" style={styles.boton}></Button> */}
-        <Pressable style={styles.boton} onPress={onPress}>
-            <Text>Iniciar sesion</Text>
-        </Pressable>
-      </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.titulo}>
+        <Text style={styles.mostaza}>Easy</Text>
+        <Text style={styles.verde}>Eating</Text>
+      </Text>
+
+      <Text style={styles.subtitulo}>Escaneá, conocé, disfrutá</Text>
+
+      <TouchableOpacity style={styles.botonIniciarSesion}>
+        <Text style={styles.iniciarSesion}>Iniciar Sesión</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={styles.botonRegistrarse}>
+        <Text style={styles.registrarse}>Registrarse</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
 
-    boton:{ 
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: 'black',
-    },
+  botonRegistrarse: {
+    elevation: 8,
+    backgroundColor: "#D1881A",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
 
-    container: {
-        padding: 80,
-        marginTop: Constants.statusBarHeight,
-    },
-    
-    titulo: {
-      fontWeight: 'bold',
-      fontSize: 40,
-      paddingBottom: 5,
-      textAlign: 'center'
-    },
-  
-    subtitulo:{
-      fontStyle: 'italic',
-      textAlign: 'center',
-      paddingBottom: 50
-    },
-  
-    mostaza: {
-      color: '#D1881A'
-    },
-  
-    verde: {
-      color: '#119B48'
-    },
-  
-    boton: {
-      
-    },
-  })
+  registrarse: {
+    fontSize: 11,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    fontFamily: 'Inter Regular'
+  },
 
-export default Main
+  iniciarSesion: {
+    fontSize: 11,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    fontFamily: 'Inter Regular',
+  },
+
+  botonIniciarSesion: {
+    elevation: 8,
+    backgroundColor: "#119B48",
+    borderRadius: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+
+  container: {
+    padding: 80,
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: '#FFFAF3',
+    flex: 1
+  },
+
+  titulo: {
+    fontWeight: "bold",
+    fontSize: 40,
+    paddingBottom: 5,
+    textAlign: "center",
+    fontFamily: 'Fredoka SemiBold'
+  },
+
+  subtitulo: {
+    fontStyle: "italic",
+    textAlign: "center",
+    paddingBottom: 20,
+    fontFamily: 'Inter Regular',
+    fontSize: 14
+  },
+
+  mostaza: {
+    color: "#D1881A",
+  },
+
+  verde: {
+    color: "#119B48",
+  },
+});
+
+export default Main;
